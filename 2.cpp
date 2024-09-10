1146a6fec96f3ae08143c97963f259b642aed07a
aaalk <115035998+aaalk@users.noreply.github.com>
2024-09-10 15:53:35 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double a = 0, b = 0;
+    cout << "Введите 2 числа a и b: " << endl;
+    cin >> a >> b;
+    cout << "Сумма = " << a + b << "\n";
+    cout << "Разность a и b = " << a - b << "\n";
+    cout << "Разность b и a = " << b - a << "\n";
+    cout << "Произведение = " << a * b << "\n";
+    if (b != 0) {
+        cout << "Частное a и b = " << a / b << "\n";
+    }
+    else {
+        cout << "Делить на 0 нельзя" << "\n";
+    }
+    if (a != 0) {
+        cout << "Частное b и a = " << b / a << "\n";
+    }
+    else {
+        cout << "Делить на 0 нельзя" << "\n";
+    }
+}
+
---
+++ DZ_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Неклеенова Елена";
+}
